c65871e96640127b8945c7d1fc8346fcabec4a23
unknown <naba.nahith@gmail.com>
2019-12-12 13:40:59 +0300

    v1

---
+++ precache-manifest.314aa5acc91bf22580ba0cd3087ac12d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c143bfc4492920203c3bd09f80ce241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab44f158dbe8cebd9ac6",
+    "url": "/static/css/main.6a7530e9.chunk.css"
+  },
+  {
+    "revision": "3e8f2d96ca5959e64633",
+    "url": "/static/js/2.d969db37.chunk.js"
+  },
+  {
+    "revision": "ab44f158dbe8cebd9ac6",
+    "url": "/static/js/main.7ca0ed3f.chunk.js"
+  },
+  {
+    "revision": "a065c5cf02d018d2dc27",
+    "url": "/static/js/runtime-main.db9b0e5f.js"
+  }
+]);
